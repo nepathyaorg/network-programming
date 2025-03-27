fbb4de3cc005f495f398d4cee608d677c402cda1
Neesha <nishabhattarai778@gmail.com>
2025-03-27 16:01:34 +0545

    Assignment added

---
+++ Nisha_Bhattarai/Programs/Unit3/Program2.java
@@ -0,0 +1,24 @@
+package Nisha_Bhattarai.Programs.Unit3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+
+public class Program2 {
+    public static void main(String[] args) {
+        try {
+            URL url = new URI("https://www.wikipedia.org/").toURL();
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println("Error retrieving data: " + e.getMessage());
+        }
+    }
+}
---
+++ Nisha_Bhattarai/Programs/Unit3/Program4.java
@@ -0,0 +1,23 @@
+package Nisha_Bhattarai.Programs.Unit3;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class Program4 {
+    public static void main(String[] args) {
+        try {
+            String name ="Hello world@";
+            String encoder=URLEncoder.encode(name,"UTF-8");
+            System.out.println("Encoding Text"+encoder);
+            String Decoder=URLDecoder.decode(name, "UTF-8");
+            System.out.println("Decoding text"+Decoder);
+
+
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
