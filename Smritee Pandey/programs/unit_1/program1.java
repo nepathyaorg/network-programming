423705d75a89907340254d65b03e302e5270f65e
Smriti <ishupandey0312@gmail.com>
2025-04-16 09:08:27 +0545

    assignment submitted

---
+++ Smritee Pandey/programs/unit_1/program1.java
@@ -0,0 +1,44 @@
+package programs.unit_1;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class program1{
+    public static void main(String[] args) {
+        try{
+            InetAddress iaddress = InetAddress.getByName("224.1.1.1");
+            if(iaddress.isAnyLocalAddress()){
+                System.out.println(iaddress+"is a local address.");
+            }
+            if(iaddress.isLoopbackAddress()){
+                System.out.println(iaddress+"is loopback address.");
+            }
+            if(iaddress.isLinkLocalAddress()){
+                System.out.println(iaddress+"is a link-local address.");
+            }else if(iaddress.isSiteLocalAddress()){
+                System.out.println(iaddress+"is a site-local address.");
+            }else{
+                System.out.println(iaddress+"is a global address.");
+            }
+            // multicast network
+            if(iaddress.isMulticastAddress()){
+                if(iaddress.isMCGlobal()){
+                    System.out.println(iaddress+"is a global multicast address.");
+                }else if(iaddress.isMCOrgLocal()){
+                    System.out.println(iaddress+"is a organizational wide multicast address.");
+                }else if(iaddress.isMCSiteLocal()){
+                    System.out.println(iaddress+"is a site wide multicast address.");
+                }else if(iaddress.isMCLinkLocal()){
+                    System.out.println(iaddress+"is a subnet wide multicast address.");
+                }else if(iaddress.isMCNodeLocal()){
+                    System.out.println(iaddress+"is an interface-local multicast address.");
+                }else{
+                    System.out.println(iaddress+"is a unicast address.");
+                }
+            }
+
+        }catch(UnknownHostException e){
+            System.out.println("could not resolve."+args[0]);
+        }
+        
+    }
+}
---
+++ Smritee Pandey/programs/unit_1/program2.java
@@ -0,0 +1,34 @@
+package programs.unit_1;
+import java.net.InetAddress;
+public class program2 {
+    public static void main(String[] args) {
+        try{
+        InetAddress inetaddressins = InetAddress.getByName("facebook.com");
+        String hostname=inetaddressins.getHostName();
+        String hostaddress = inetaddressins.getHostAddress();
+        String canonicalhostname = inetaddressins.getCanonicalHostName();
+        System.out.println(hostname);
+        System.out.println(hostaddress);
+        System.out.println(canonicalhostname);  
+        // localhost 
+        InetAddress addressins = InetAddress.getLocalHost();
+        String localhost = addressins.getHostName();
+        String localhostaddress = addressins.getHostAddress();
+        System.out.println(localhost);
+        System.out.println(localhostaddress);
+        byte[] hfjsk = inetaddressins.getAddress();
+        System.out.println(hfjsk);
+        if(hfjsk.length==4){
+            System.out.println("ipv4");
+        }
+        else if(hfjsk.length==6){
+            System.out.println("ipv6");
+        }
+        else{
+            System.out.println("invalid input.");
+        }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
